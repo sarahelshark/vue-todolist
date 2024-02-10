92f2feadbdd66eb2f3dce3185b7fd3dbdc4a6849
sarahelshark <sarahelsh183@gmail.com>
2024-02-10 14:37:08 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script >
+export default{
+  name:'App',
+  data(){
+    return {
+      message:'hello'
+
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<style >
+</style>
